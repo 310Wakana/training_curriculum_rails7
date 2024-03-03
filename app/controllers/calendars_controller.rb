fa2104fe7438852a305d81e483aa80e5c9f80d1d
310Wakana <wakana25.aaa@gmail.com>
2024-03-03 12:20:08 +0900

    Issue6の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -42,10 +42,17 @@ class CalendarsController < ApplicationController
         wday_num = wday_num - 7
       end
 
+<<<<<<< HEAD
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
+=======
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num]}
 
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans }
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> b7856ea3e83196ad735eeb946a0e8372bb813c8d
+>>>>>>> Stashed changes
       @week_days.push(days)
     end
   end
